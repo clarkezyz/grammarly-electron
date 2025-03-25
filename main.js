5e880bd94dc08d88bb4b355b14e0df84a1453425
clarkezyz <cc@zyzd.cc>
2025-03-25 03:51:57 -0700

    Initial commit: Grammarly Electron app wrapper

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  })
+
+  // Load the website URL
+  win.loadURL('https://app.grammarly.com/')
+  
+  // Optional: Hide the menu bar for a cleaner look
+  win.setMenuBarVisibility(false)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
